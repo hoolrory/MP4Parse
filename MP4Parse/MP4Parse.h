18336beef5c1e73f6d1a035670b5d7aa5a5a1f40
Hool, Rory <hoolrory@gmail.com>
2016-07-24 12:10:52 -0400

    Replace MP4Parse.xcodeproj with "Cocoa Touch Static Library" Xcode Project

---
+++ MP4Parse/MP4Parse.h
@@ -0,0 +1,13 @@
+//
+//  MP4Parse.h
+//  MP4Parse
+//
+//  Created by Hool, Rory on 7/24/16.
+//  Copyright © 2016 RoryHool. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MP4Parse : NSObject
+
+@end
---
+++ MP4Parse/MP4Parse.m
@@ -0,0 +1,13 @@
+//
+//  MP4Parse.m
+//  MP4Parse
+//
+//  Created by Hool, Rory on 7/24/16.
+//  Copyright © 2016 RoryHool. All rights reserved.
+//
+
+#import "MP4Parse.h"
+
+@implementation MP4Parse
+
+@end
