c379b35db19f8a03fe810c32abbfe26362a8c2be
Hool, Rory <hoolrory@gmail.com>
2016-08-10 22:27:50 -0400

    Add subtitle method override to UnknownAtom

--- source/include/MP4.UnknownAtom.h
+++ source/include/MP4.UnknownAtom.h
@@ -74,7 +74,8 @@ namespace MP4
             
             UnknownAtom( void );
             UnknownAtom( char * t );
-            
+        
+            std::string subtitle() { return "Unknown"; }
             std::string description( int depth );
             void processData( MP4::BinaryStream * stream, size_t length );
     };
